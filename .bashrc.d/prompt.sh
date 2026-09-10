155d7f7cf22c06a72d9816f4b4ab5fac40064e67
Antti Nilakari <antti.nilakari@iki.fi>
2026-09-10 15:01:49 +0300

    Simplify prompt

--- .bashrc.d/prompt.sh
+++ .bashrc.d/prompt.sh
@@ -1,20 +1,5 @@
 __bash_git_branch () {
     if (git rev-parse --is-inside-work-tree | grep true) >/dev/null 2>&1; then
-        FETCH_HEAD="$(git rev-parse --git-path FETCH_HEAD)"
-        if [ -f "${FETCH_HEAD}" ]; then
-            FETCHED_AT=$(stat -c %Y "${FETCH_HEAD}")
-        else
-            FETCHED_AT=0
-        fi
-        NOW=$(date +%s)
-        if [ $(($NOW - $FETCHED_AT)) -ge 900 ]; then
-            tput sc
-            echo -en "" && tput civis; tput cnorm
-            git fetch --no-tags --prune --filter=blob:none >/dev/null 2>&1;
-            tput rc
-            tput el
-        fi
-
         BRANCH=$(git symbolic-ref --short HEAD 2>/dev/null || git describe --tags --exact-match 2>/dev/null || git rev-parse --short HEAD 2>/dev/null)
         STATUS=$(git status --porcelain=v2 --branch --show-stash)
 
@@ -29,7 +14,7 @@ __bash_git_branch () {
         AHEAD="$(echo " $(echo "${STATUS}" | grep -F "# branch.ab" | cut -d" " -f3)" | grep -- "+[1-9]")"
         BEHIND="$(echo " $(echo "${STATUS}" | grep -F "# branch.ab" | cut -d" " -f4)" | grep -- "-[1-9]")"
         STASH="$(echo " ~$(echo "${STATUS}" | grep -F "# stash" | cut -d" " -f3)" | grep -- "[1-9]")"
-        echo -e "\[\e[38;5;214m\]⌥ ${BRANCH}${DIRTY}\[\e[1;32m\]${AHEAD}\[\e[0m\]\[\e[1;31m\]${BEHIND}\[\e[0m\]\[\e[1;34m\]${STASH}\[\e[0m\]"
+        echo -e "\[\e[38;5;214m\]:${BRANCH}${DIRTY}\[\e[1;32m\]${AHEAD}\[\e[0m\]\[\e[1;31m\]${BEHIND}\[\e[0m\]\[\e[1;34m\]${STASH}\[\e[0m\]"
     fi
 }
 
@@ -37,7 +22,7 @@ __bash_kube_context () {
     CONTEXT=$(kubectl config current-context 2>/dev/null)
     if [ $? -eq 0 ]; then
         NAMESPACE=$(kubectl config view --minify -o jsonpath='{..namespace}')
-        echo -e "\[\e[1;38;5;33m\]⎈ $CONTEXT/$NAMESPACE\[\e[0m\]"
+        echo -e "\[\e[1;38;5;33m\]$CONTEXT/$NAMESPACE\[\e[0m\]"
         return 0
     else
         return 1
@@ -71,11 +56,10 @@ __bash_prompt () {
     GIT_BRANCH="$(__bash_git_branch)"
     PS1="${EXIT_STATUS}${KUBE_CONTEXT}:\[\033[01;34m\]\W${GIT_BRANCH}\[\033[00m\]\[\e[91m\]\[\e[00m\]$ "
 
-    prompt_command__isnewline__last="$prompt_command__isnewline__curr"
-    prompt_command__isnewline__curr="$(history 1 | sed 's/^[[:space:]]*[0-9]\+[[:space:]]*//')"
-    if [ "$prompt_command__isnewline__curr" = "$prompt_command__isnewline__last" ]; then
+    if [[ ${_last_histcmd:-$HISTCMD} == "$HISTCMD" ]]; then
         PS1=$(echo "$PS1" | sed 's/\\W/\\w/g')
     fi
+    _last_histcmd=$HISTCMD
 }
 
-export PROMPT_COMMAND=__bash_prompt
+PROMPT_COMMAND=__bash_prompt
